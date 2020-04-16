07e62ce34937a647a245c4dff4e39922e52acdd6
Thang Nguyen <nhthang.khtn@gmail.com>
2020-04-16 12:10:09 +0700

    Update gradle and change the UI to make sure 3 items have same UI and style

--- app/src/main/java/com/krpiotrek/constraintlayoutstuff/MainActivity.java
+++ app/src/main/java/com/krpiotrek/constraintlayoutstuff/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.item_new);
+        setContentView(R.layout.activity_main);
     }
 }

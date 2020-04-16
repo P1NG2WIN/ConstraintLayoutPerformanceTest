aa0da95ddff39eb31fa07419525b9348d0be7693
Thang Nguyen <nhthang.khtn@gmail.com>
2020-04-16 14:54:19 +0700

    Calculate time for each step inflate, measure and layout, beautify time report

--- app/src/androidTest/java/com/krpiotrek/constraintlayoutstuff/PerformanceTest.java
+++ app/src/androidTest/java/com/krpiotrek/constraintlayoutstuff/PerformanceTest.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.test.filters.SmallTest;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
@@ -18,29 +19,75 @@ public class PerformanceTest {
 
     @Test
     public void test() {
-        final long relativeLayoutTime = getLayoutTime(R.layout.item_old_relative);
-        final long linearLayoutTime = getLayoutTime(R.layout.item_old_linear);
-        final long constraintLayoutTime = getLayoutTime(R.layout.item_new);
+        // Warn up, make the result more accurate
+        getLayoutTime(R.layout.activity_main);
 
-        Log.i("time", "constraint : " + constraintLayoutTime);
-        Log.i("time", "linear : " + linearLayoutTime);
-        Log.i("time", "relative : " + relativeLayoutTime);
+        // Benchmark
+        final BMTime linearLayoutTime = getLayoutTime(R.layout.item_old_linear);
+        final BMTime relativeLayoutTime = getLayoutTime(R.layout.item_old_relative);
+        final BMTime constraintLayoutTime = getLayoutTime(R.layout.item_new);
+
+        Log.i("time", "Linear    :\t" + linearLayoutTime);
+        Log.i("time", "Relative  :\t" + relativeLayoutTime);
+        Log.i("time", "Constraint:\t" + constraintLayoutTime);
     }
 
-    private long getLayoutTime(int layoutRes) {
+    private BMTime getLayoutTime(int layoutRes) {
         final LayoutInflater layoutInflater = LayoutInflater.from(InstrumentationRegistry.getInstrumentation().getTargetContext());
 
-        final long startTime = System.nanoTime();
+        long inflateTime = 0;
+        long measureTime = 0;
+        long layoutTime = 0;
         for (int i = 0; i < 1000; i++) {
+            TimeCounter.start();
             final View view = layoutInflater.inflate(layoutRes, null);
-            view.setLayoutParams(new ViewGroup.LayoutParams(0, 0));
+            inflateTime += TimeCounter.count();
 
+            view.setLayoutParams(new ViewGroup.LayoutParams(0, 0));
             view.measure(View.MeasureSpec.makeMeasureSpec(1000, View.MeasureSpec.EXACTLY), View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
             final int measuredHeight = view.getMeasuredHeight();
             final int measuredWidth = view.getMeasuredWidth();
+            measureTime += TimeCounter.count();
 
             view.layout(0, 0, measuredWidth, measuredHeight);
+            layoutTime += TimeCounter.count();
         }
-        return System.nanoTime() - startTime;
+        return new BMTime(inflateTime, measureTime, layoutTime);
+    }
+
+    static class TimeCounter {
+        private static long time;
+
+        static void start() {
+            time = System.nanoTime();
+        }
+
+        static long count() {
+            long now = System.nanoTime();
+            long count = now - time;
+            time = now;
+            return count;
+        }
+    }
+
+    static class BMTime {
+        long inflateTime;
+        long measureTime;
+        long layoutTime;
+
+        BMTime(long inflateTime, long measureTime, long layoutTime) {
+            this.inflateTime = inflateTime;
+            this.measureTime = measureTime;
+            this.layoutTime = layoutTime;
+        }
+
+        @NonNull
+        @Override
+        public String toString() {
+            return "inflate:\t" + inflateTime +
+                    "\tmeasure:\t" + measureTime +
+                    "\tlayout:\t" + layoutTime +
+                    "\ttotal:\t" + (inflateTime + measureTime + layoutTime);
+        }
     }
 }
